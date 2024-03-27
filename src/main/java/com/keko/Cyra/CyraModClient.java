46ea075779b02bb38a9208b675b3ba352b52148a
Salsa <andrei.pencu@outlook.com>
2024-03-28 00:53:23 +0200

    setup

---
+++ src/main/java/com/keko/Cyra/CyraModClient.java
@@ -0,0 +1,11 @@
+package com.keko.Cyra;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+
+public class CyraModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient(ModContainer mod) {
+
+	}
+}
